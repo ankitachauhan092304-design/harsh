4652bc43bcba520a94a847ac64704cb73178efaa
ankitachauhan092304-design <ankita.chauhan092304@gmail.com>
2026-08-24 18:46:49 +0530

    Updates

--- src/config/crmConfig.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_GOOGLE_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbz0cUzmV5xLrHAG90ECaM1RtYvvFXPn6Qo0cQVE3uNp-6SX6VsfHpeNq1FzdtIdnSbZ/exec';
-export const DEFAULT_WA_NUMBER = '919824975488';
